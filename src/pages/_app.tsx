7944e7579aff49bf815ad72e74198a753ee7d68d
Shreyas Sahoo <shreyassahoo26@gmail.com>
2023-08-21 09:31:43 +0530

    added basic sidebar and updated layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,27 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-
+import Head from "next/head";
+import SideBar from "~/components/SideBar";
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>X Clone</title>
+        <meta
+          name="description"
+          content="This is a X Clone build with the T3 Stack"
+        />
+      </Head>
+      <div className="container mx-auto flex">
+        <SideBar />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
